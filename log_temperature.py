6c4b1e23f28203822a5534b052ca5b86cfdc3a90
Kris Mitchell <kris.r.m@gmail.com>
2016-09-19 21:20:47 -0600

    fixed an issue where a google account error would crash the monitor

--- log_temperature.py
+++ log_temperature.py
@@ -139,7 +139,7 @@ def login_open_sheet(oauth_key_file, spreadsheet):
     except Exception as ex:
         print('Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
         print('Google sheet login failed with error:', ex)
-        sys.exit(1)
+        return None
 
 
 print('Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS))
@@ -151,6 +151,11 @@ while True:
     # Login if necessary.
     if worksheet is None:
         worksheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME)
+
+    #don't crash if we didn't login right
+    if worksheet is None:
+        time.sleep(FREQUENCY_SECONDS)
+        continue
 
     # Attempt to get sensor reading.
     humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
